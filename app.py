330ce00ff5b2848cc0cc5516dd08a2e4cd5704e7
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2021-04-14 19:20:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def Covid_predict():
 #   p20 =data["p20"]
 
 #   res = predict(0,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20)
-res = predict(0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1)
+  res = predict(0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1)
   result = {
                 'covid_infected': res
             }
