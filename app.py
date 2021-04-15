ff1f9028cf41ecc57f04d21f548465900f4067cd
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2021-04-15 18:37:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,11 @@
 from flask import Flask, request, jsonify
-import pandas as pd
 import pickle
-import numpy as np
+
 app = Flask(__name__)
 
 
-def predict(Breathing_Problem, Fever, Dry_Cough, Sore_throat, Running_Nose, Asthma, Chronic_Lung_Disease, Headache,
-            Heart_Disease, Diabetes, Hyper_Tension, Fatigue, Gastrointestinal, Abroad_travel,
-            Contact_with_COVID_Patient, Attended_Large_Gathering, Visited_Public_Exposed_Places,
-            Family_working_in_Public_Exposed_Places, Wearing_Masks, Sanitization_from_Market):
- 
 
-  with open("Covid_Model.pkl", 'rb') as file:
-    model = pickle.load(file)
-
-  return model.predict(np.array([[Breathing_Problem, Fever, Dry_Cough, Sore_throat, Running_Nose, Asthma,
-                                  Chronic_Lung_Disease, Headache, Heart_Disease, Diabetes, Hyper_Tension, Fatigue,
-                                  Gastrointestinal, Abroad_travel, Contact_with_COVID_Patient, Attended_Large_Gathering,
-                                  Visited_Public_Exposed_Places, Family_working_in_Public_Exposed_Places, Wearing_Masks,
-                                  Sanitization_from_Market]]))
-
-
-@app.route("/covid_predict/",methods=['GET'])
+@app.route("/covid_predict",methods=['GET'])
 def Covid_predict():
 
 #   data=request.get_json();
@@ -47,6 +31,8 @@ def Covid_predict():
 #   p20 =data["p20"]
 
 #   res = predict(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20)
+  import pandas as pd
+  import numpy as np
   dataset=pd.read_csv("COVID.csv")
   X=dataset.iloc[:,:-1].values
   Y = dataset.iloc[:,-1].values
@@ -61,10 +47,8 @@ def Covid_predict():
   Y = np.asarray(Y)
   
   from sklearn.linear_model import LogisticRegression
-  from sklearn.model_selection import train_test_split
   logreg = LogisticRegression()
   logreg.fit(X, Y)
-
   res = logreg.predict([[1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
   return str(res)
   
