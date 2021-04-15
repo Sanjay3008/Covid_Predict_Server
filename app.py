f4647f2a940c63d40747da5c81ecf41ce12f7c24
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2021-04-15 20:18:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ app.logger.setLevel(logging.ERROR)
 
 def covid_pred(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20):
   
-  dataset=pd.read_csv("COVID.csv")
+  dataset=pd.read_csv("data/COVID.csv")
   X=dataset.iloc[:,:-1].values
   Y = dataset.iloc[:,-1].values
-  return "HI"
+ 
             
   from sklearn.preprocessing import LabelEncoder
   encoder = LabelEncoder()
