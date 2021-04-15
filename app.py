9d2a4500501b9d32dceca1a1faf8e9b5bfc66899
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2021-04-15 19:39:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from flask import Flask, request, jsonify
 import pickle
 
 app = Flask(__name__)
+import logging
 
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 def covid_pred(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20):
   dataset=pd.read_csv("COVID.csv")
