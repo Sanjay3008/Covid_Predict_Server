19279ad5bbe60aff5db82fb2382cb97d0ac8c0d7
S Sanjay <58475664+Sanjay3008@users.noreply.github.com>
2021-04-16 16:18:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,8 @@ def Covid_predict():
 
 #   res = covid_pred(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20)
   res = covid_pred(1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1)
-  return str(res[0])
+  result = { "covid": res[0] }
+  return result
   
 @app.route("/",methods=['GET'])
 def default():
